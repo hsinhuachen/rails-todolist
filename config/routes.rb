e27b06cd418d40a1319d1703476b6c25c352fd75
hsinhuachen <yha0971@gmail.com>
2020-03-14 23:56:01 +0800

    Add user login

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'applicants/new'
+  get 'applicants/create'
+  post 'applicants/create'
+  post 'sessions/create'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
